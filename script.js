95c2fa4f5faae0a082f7e4d7b3170f7aa699b3d1
Faijan-Momin <faijanmomin99@gmail.com>
2021-05-01 12:44:45 +0530

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function changeLike(){
-    var likebtn = document.querySelector('.meme-like-btn');
-    likebtn.style.backgroundImage = "url('content/heart-red.svg')";
-    // if(likebtn == "url('content/heart.svg')"){
-    //     likebtn = "url('content/heart-red.svg')";
-    // }
-    // else{
-    //     likebtn = "url('content/heart.svg')";
-    // }
-    
-}
+// function changeLike(lb){
+//     lb.classList.toggle("meme-like-btn-liked");
+// }
